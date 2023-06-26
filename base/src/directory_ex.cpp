893c7eb76975d9b15cd40b32de24726328cb5c4d
liyiming13 <liyiming13@huawei.com>
2023-06-26 15:11:00 +0800

    Fix failing to remove a dangling symlink file in ForceRemoveDirectory
    1. Replace `access` to `faccessat`
    2. Add related test cases.
    
    Issue:I7FUB2
    Test:UT
    
    Signed-off-by: liyiming13 <liyiming13@huawei.com>

--- base/src/directory_ex.cpp
+++ base/src/directory_ex.cpp
@@ -15,7 +15,8 @@
 
 #include "directory_ex.h"
 #include <dirent.h>
-#include <errno.h>
+#include <cerrno>
+#include <fcntl.h>
 #include "securec.h"
 #include "unistd.h"
 #include "utils_log.h"
@@ -154,7 +155,7 @@ bool ForceRemoveDirectory(const string& path)
         if (ptr->d_type == DT_DIR) {
             ret = ForceRemoveDirectory(subPath);
         } else {
-            if (access(subPath.c_str(), F_OK) == 0) {
+            if (faccessat(AT_FDCWD, subPath.c_str(), F_OK, AT_SYMLINK_NOFOLLOW) == 0) {
                 if (remove(subPath.c_str()) != 0) {
                     closedir(dir);
                     return false;
@@ -165,13 +166,13 @@ bool ForceRemoveDirectory(const string& path)
     closedir(dir);
 
     string currentPath = ExcludeTrailingPathDelimiter(path);
-    if (access(currentPath.c_str(), F_OK) == 0) {
+    if (faccessat(AT_FDCWD, currentPath.c_str(), F_OK, AT_SYMLINK_NOFOLLOW) == 0) {
         if (remove(currentPath.c_str()) != 0) {
             return false;
         }
     }
 
-    return ret && (access(path.c_str(), F_OK) != 0);
+    return ret && (faccessat(AT_FDCWD, path.c_str(), F_OK, AT_SYMLINK_NOFOLLOW) != 0);
 }
 
 bool RemoveFile(const string& fileName)
