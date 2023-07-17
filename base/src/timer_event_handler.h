a84abb668bef7da28ccf072e9c465b869962a845
huangyuchen <huangyuchen3@huawei.com>
2023-07-17 17:43:19 +0800

    Adjusts partial structure of timer to fix the UAF issue furtherly.
    Issue: I7LRLM
    Test: Fuzz test on version built with libfuzz and asan.
    
    Signed-off-by: huangyuchen <huangyuchen3@huawei.com>
    Change-Id: I01c42205e5887087a14243ee7efb46098473d8c8

--- base/src/timer_event_handler.h
+++ base/src/timer_event_handler.h
@@ -18,17 +18,17 @@
 #include <functional>
 #include <memory>
 #include <cstdint>
+#include "event_handler.h"
 
 namespace OHOS {
 namespace Utils {
 constexpr int INVALID_TIMER_FD = -1;
 
-class EventHandler;
 class EventReactor;
 
-class TimerEventHandler {
+class TimerEventHandler : public EventHandler {
     using TimerCallback = std::function<void(int timerFd)>;
-    
+
 public:
     TimerEventHandler(EventReactor* p, uint32_t timeout, bool once);
     ~TimerEventHandler();
@@ -44,19 +44,14 @@ public:
     void SetTimerCallback(const TimerCallback& callback) { callback_ = callback; }
 
     uint32_t GetInterval() const { return interval_; }
-    int GetTimerFd() const { return timerFd_; }
 
 private:
     void TimeOut();
 
 private:
     bool           once_;
-    int            timerFd_;
     uint32_t       interval_;
-    EventReactor*  reactor_;
-
-    std::shared_ptr<EventHandler> handler_;
-    TimerCallback                 callback_;
+    TimerCallback  callback_;
 };
 
 } // namespace Utils
